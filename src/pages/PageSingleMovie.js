8dd0026fdad51a4d93b04da5b44bd86fa28a7830
aysenurdev <117058195+aysenurdev@users.noreply.github.com>
2024-02-09 20:11:15 -0800

    Single page is updated

--- src/pages/PageSingleMovie.js
+++ src/pages/PageSingleMovie.js
@@ -26,29 +26,35 @@ function PageSingleMovie() {
         <div className="single-movie-page">
             {movieData && (
                 <>
-                    <div className="single-movie-header">
-                        <h1>{movieData.title}</h1>
-                        <div className="single-movie-details">
-                            <p>{formatReleaseDate(movieData.release_date)}</p>
-                            <FavoriteButton movieData={movieData} />
-                        </div>
-                    </div>
+                    
                     <div className="single-movie-content">
                         <div className="single-poster">
-                            <img className="backside"   src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt={movieData.title} />
-                            <img  className="poster-front"   src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt={movieData.title} />
+                            <img className="backside"   src={`https://image.tmdb.org/t/p/w1280/${movieData.poster_path}`} alt={movieData.title} />
                         </div>
+
+                
                         <div className="single-movie-info">
-                            <p>{movieData.overview}</p>
-                            {movieVideos.length > 0 && (
+                        <img  className="poster-front"   src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt={movieData.title} />
+                           
+                        <div className="single-movie-header">
+                        <h1>{movieData.title}</h1>
+
+                        <p>{formatReleaseDate(movieData.release_date)}</p>
+                            
+                        
+                        <p>{movieData.overview}</p>
+                        <FavoriteButton movieData={movieData} />
+                        {movieVideos.length > 0 && (
                                 <iframe
-                                    width="640"
-                                    height="480"
+                                    width="350"
+                                    height="200"
                                     title={movieVideos[0].name}
                                     src={`https://www.youtube.com/embed/${movieVideos[0].key}`}
                                 ></iframe>
                             )}
+                        </div>  
                         </div>
+
                     </div>
                 </>
             )}
